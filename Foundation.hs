9a2494c228dbd98dd0ccbf67f6bdda650081197f
rnons <remotenonsense@gmail.com>
2013-12-10 18:18:21 +0800

    Integrate search, can search with talk url and keywords now.

--- Foundation.hs
+++ Foundation.hs
@@ -7,6 +7,8 @@
 module Foundation where
 
 import qualified Control.Exception.Lifted as E
+import           Control.Monad (forM)
+import qualified Data.ByteString.Char8 as B8
 import           Data.Monoid ((<>))
 import           Data.Text (Text)
 import qualified Data.Text as T
@@ -15,7 +17,7 @@ import           Database.Persist.Sql (SqlPersistT)
 import qualified Filesystem.Path.CurrentOS as FS
 import           Prelude hiding (id)
 import           System.Directory
-import           Text.Blaze.Internal (Markup)
+import           Text.Blaze.Internal (preEscapedText)
 import           Text.Jasmine (minifym)
 import           Text.Julius (rawJS)
 import           Yesod hiding (languages)
@@ -25,8 +27,8 @@ import           Yesod.Static
 import Model
 import Settings
 import Ted
-import Ted.Types (Talk(..), SubTalk(..))
-import Ted.Fallback (getTalk)
+import Ted.Types (Talk(..), SubTalk(..), SearchTalk(..))
+import Ted.Fallback (getTalk, getSlugAndPad)
 
 
 data Ted = Ted
@@ -43,6 +45,7 @@ mkYesod "Ted" [parseRoutes|
 /play PlayR GET
 /watch WatchR GET
 /talks/#Text TalksR GET
+/search/#Text SearchR GET
 |]
 
 instance Yesod Ted where
@@ -76,50 +79,31 @@ instance YesodPersist Ted where
 instance RenderMessage Ted FormMessage where
     renderMessage _ _ = defaultFormMessage
 
-talkForm :: Markup -> MForm (HandlerT Ted IO) (FormResult Text, Widget)
-talkForm = renderDivs $ areq talkUrlField ""
-                                         { fsId = Just "search_input"
-                                         , fsName = Just "q"
-                                         , fsAttrs = [ ("placeholder", "URL")
-                                                     , ("autofocus", "true")
-                                                     ]
-                                         } Nothing
-  where
-    errorMessage :: Text
-    errorMessage = "Valid url is like this http://www.ted.com/talks/thomas_heatherwick.html"
-    
-    talkUrlField = check validUrl textField
-    validUrl x
-        | talkUrl `T.isPrefixOf` x = Right x
-        | otherwise                = Left errorMessage
-
 getFaviconR :: MonadHandler m => m ()
 getFaviconR = sendFile "image/x-icon" "favicon.ico"
 
 -- Homepage consists talkForm and talkTile
--- Input valid TED talk page url into talkForm, all avialable subtitles
--- will be returned.
+-- talkForm accepts talk url or keywords, redirct to TalkR or SearchR
 -- talkTile displays five recent talks. There is a cron job running per 12
 -- hours to fetch the recent talks. (fetch.hs)
 getHomeR :: Handler Html
 getHomeR = do
-    ((result, widget), enctype) <- runFormGet talkForm
-    case result of
-        FormSuccess q -> redirect $ TalksR $ T.drop (T.length talkUrl) q
-        _       -> do
+    q <- lookupGetParam "q"
+    case q of
+        Just kw
+            | talkUrl `T.isPrefixOf` kw -> 
+                redirect $ TalksR $ T.drop (T.length talkUrl) kw
+            | otherwise -> redirect $ SearchR kw
+        _ -> do
             talks' <- E.catch (runDB $ selectList [] [Desc TalkTid, LimitTo 5])
                               (\e -> liftIO $ print (e :: E.SomeException) >> 
                                return [])
             let talks = flip map talks' $ \(Entity _ t) ->
-                    t { talkLink = rewriteUrl $ talkLink t }
+                        t { talkLink = rewriteUrl $ talkLink t }
             defaultLayout $ do
                 setTitle "Ted2srt: Download bilingual subtitles of TED talks | Subtitles worth spreading"
                 toWidgetHead [hamlet| <meta name=description content="Find out all available subtitle languages, download as plain text or srt file. Watch TED talks with bilingual subtitle. TED演讲双语字幕下载。">|]
                 $(widgetFile "homepage")
-  where
-    -- substitute ted.com to ted2srt.org  e.g.
-    -- http://www.ted.com/talks/marla_spivak_why_bees_are_disappearing.html
-    rewriteUrl url = "http://ted2srt.org" <> T.drop 18 url
 
 getDownloadR :: Handler RepPlain
 getDownloadR = do
@@ -181,12 +165,36 @@ getWatchR = do
                 $(widgetFile "watch")
          _             -> redirect HomeR
 
+getSearchR :: Text -> Handler Html
+getSearchR q = do
+    searchtalks <- liftIO $ searchTalk $ B8.pack $ T.unpack q
+    dbtalks <- forM searchtalks $ \t -> do
+        mtalk <- runDB $ selectFirst [TalkTid ==. s_id t] []
+        case mtalk of 
+            Just (Entity _ talk') -> return talk'
+            _                    -> do
+                tedtalk <- liftIO $ queryTalk $ s_id t
+                (slug, pad) <- liftIO $ getSlugAndPad $ tedTalkUrl $ s_slug t
+                let dbtalk = Model.Talk { talkTid = _id tedtalk
+                                        , talkTitle = _name tedtalk
+                                        , talkDescription = s_description t
+                                        , talkLink = tedTalkUrl $ s_slug t
+                                        , talkImage = talkImg tedtalk
+                                        , talkMediaSlug = slug
+                                        , talkMediaPad = pad
+                                        }
+                runDB $ insertUnique dbtalk
+                return dbtalk
+                                        
+    let talks = flip map dbtalks $ \t ->
+                t { talkLink = rewriteUrl $ talkLink t }
+            
+    defaultLayout $ do
+        $(widgetFile "topbar")
+        $(widgetFile "search")
+
 getTalksR :: Text -> Handler Html
 getTalksR url = do
-    ((result, widget), _) <- runFormGet talkForm
-    q' <- case result of
-         FormSuccess q -> return q
-         _             -> return $ talkUrl <> url
     mtalk <- runDB $ selectFirst [TalkLink ==. url] []
     case mtalk of
         Just (Entity _ talk') -> do
@@ -200,26 +208,27 @@ getTalksR url = do
                                 , subSlug = talkMediaSlug talk'
                                 , subLag = talkMediaPad talk'
                                 }
-            layout stalk widget    
+            layout stalk
         _          -> do
-            mtalk' <- liftIO $ getTalk q'
+            mtalk' <- liftIO $ getTalk $ tedTalkUrl url
             case mtalk' of
                 Just talk -> do
                     let dbtalk = Model.Talk { talkTid = id talk
                                             , talkTitle = name talk
+                                            , talkDescription = description talk
                                             , talkLink = link talk
                                             , talkImage = image talk
                                             , talkMediaSlug = subSlug talk
                                             , talkMediaPad = subLag talk
                                             }
                     runDB $ insertUnique dbtalk
-                    layout talk widget
+                    layout talk
                 _         -> do
-                    let msg = "ERROR: " <> q' <> " is not a TED talk page!"
+                    let msg = "ERROR: " <> url <> " is not a TED talk page!"
                     setMessage $ toHtml msg
                     redirect HomeR
   where
-    layout talk widget = defaultLayout $ do
+    layout talk = defaultLayout $ do
         let prefix = downloadUrl <> subSlug talk 
             audio = prefix <> ".mp3"
             mu = mediaUrl $ subSlug talk
@@ -229,6 +238,7 @@ getTalksR url = do
             v320k = mu "320k"
         setTitle $ toHtml $ name talk <> " | Subtitle on ted2srt.org"
         addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
+        $(widgetFile "topbar")
         $(widgetFile "talks")
 
 talkUrl :: Text
@@ -241,3 +251,17 @@ downloadUrl = "http://download.ted.com/talks/"
 mediaUrl :: Text -> Text -> Text
 mediaUrl part quality =
   downloadUrl <> part <> "-" <> quality <> ".mp4"
+
+myTalkUrl :: Text -> Text
+myTalkUrl talkslug = "http://ted2srt.org/talks/" <> talkslug <> ".html"
+
+tedTalkUrl :: Text -> Text
+tedTalkUrl talkslug = "http://www.ted.com/talks/" <> talkslug <> ".html"
+
+img113x85 :: Text -> Text
+img113x85 url = (T.reverse. T.drop 11 . T.reverse) url <> "113x85.jpg"
+
+-- substitute ted.com to ted2srt.org  e.g.
+-- http://www.ted.com/talks/marla_spivak_why_bees_are_disappearing.html
+rewriteUrl ::Text -> Text
+rewriteUrl url = "http://ted2srt.org" <> T.drop 18 url
