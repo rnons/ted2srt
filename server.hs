cf099be03713805493f9af10175a7e64657e2c63
rnons <remotenonsense@gmail.com>
2015-03-29 18:50:49 +0800

    Add /talks/slug handler

--- server.hs
+++ server.hs
@@ -2,38 +2,93 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Control.Applicative((<$>), (<*>))
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson (decodeStrict)
-import           Data.Maybe (catMaybes, mapMaybe)
+import           Control.Monad.Trans.Either (left)
+import           Data.Aeson (encode, decodeStrict)
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Lazy as L
+import           Data.Maybe (catMaybes, mapMaybe, fromJust)
+import           Data.Monoid ((<>))
+import           Data.Text (Text)
+import qualified Data.Text as T
 import           Database.Redis hiding (decode)
 import           Network.Wai (Application)
 import           Network.Wai.Handler.Warp (run)
 import           Servant
 
+import Web.TED (getTalkId, queryTalk)
+import qualified Web.TED as API
 import Handler.Util
 
 type TedApi =
-    "newest" :> Get '[JSON] [RedisTalk]
+       "newest" :> Get '[JSON] [RedisTalk]
+  :<|> "talks" :> Capture "slug" Text :> Get '[JSON] RedisTalk
 
-getNewst :: Server TedApi
-getNewst = do
-    conn <- liftIO $ connect defaultConnectInfo
+getNewst conn = do
     emtalks <- liftIO $ runRedis conn $ do
         elatest <- lrange "latest" 0 4
-        mget $ either (const []) id elatest
+        mget $ either (const []) Prelude.id elatest
     let talks = mapMaybe decodeStrict $ catMaybes $
-                    either (const [Nothing]) id emtalks
+                    either (const [Nothing]) Prelude.id emtalks
     return talks
 
+getTalk conn slug = do 
+    emtid <- liftIO $ runRedis conn $ get $ C8.pack $ T.unpack slug
+
+    case emtid of
+        Right (Just tid) -> do
+            result <- liftIO $ runRedis conn $ multiExec $ do
+                t <- get tid
+                c <- get ("cache:" <> tid)
+                return $ (,) <$> t <*> c
+            case result of
+                TxSuccess (Just talk, Just cache) ->
+                    return (fromJust $ decodeStrict talk)
+                           -- (fromJust $ decodeStrict cache)
+                TxError _ -> left (404, "not found")
+                _ -> do
+                    talk' <- liftIO $ queryTalk $ read $ C8.unpack tid
+                    case talk' of
+                        Just talk -> do
+                            let value = apiTalkToValue talk
+                            liftIO $ runRedis conn $
+                                setex ("cache:" <> tid) (3600*24)
+                                      (L.toStrict $ encode value)
+                            getTalk conn slug
+                        Nothing   -> left (404, "not found")
+        Right Nothing    -> do
+            mtid <- liftIO $ getTalkId $ talkUrl <> slug
+            case mtid of
+                Just tid -> do
+                    talk' <- liftIO $ queryTalk tid
+                    case talk' of
+                        Nothing   -> left (404, "not found")
+                        Just talk -> do
+                            dbtalk <- liftIO $ marshal talk
+                            let value = apiTalkToValue talk
+                            liftIO $ runRedis conn $ multiExec $ do
+                                set (C8.pack $ T.unpack $ API.slug talk)
+                                    (C8.pack $ show tid)
+                                set (C8.pack $ show tid)
+                                    (L.toStrict $ encode dbtalk)
+                                setex ("cache:" <> C8.pack (show tid))
+                                      (3600*24)
+                                      (L.toStrict $ encode value)
+                            return dbtalk 
+                _         -> left (404, "not found")
+        Left _ -> left (404, "not found")
 
 tedApi :: Proxy TedApi
 tedApi = Proxy
 
-tedServer :: Server TedApi
-tedServer = getNewst
+tedServer :: Connection -> Server TedApi
+tedServer conn = getNewst conn :<|> getTalk conn
 
-app :: Application
-app = serve tedApi tedServer
+app :: Connection -> Application
+app = serve tedApi . tedServer
 
 main :: IO ()
-main = run 3001 app
+main = do
+    conn <- connect defaultConnectInfo
+    run 3001 $ app conn
