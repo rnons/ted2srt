4dc58e350adcdf20a2ca17c89e8c0c26287296dd
rnons <remotenonsense@gmail.com>
2015-06-22 15:42:29 +0800

    display transcript in talk page

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -8,6 +8,17 @@ angular.module('starter.controllers', [])
     });
 })
 
-.controller('TalkCtrl', function($scope) {
-  console.log('talk page');
+.controller('TalkCtrl', function($scope, $http, $stateParams) {
+  var slug = $stateParams.slug;
+  var talk = null;
+  $http.get('/api/talks/' + slug)
+    .then(function (response) {
+      $scope.talk = response.data.talk;
+      $scope.talk.speaker = $scope.talk.name.split(':')[0];
+      $scope.talk.title = $scope.talk.name.split(':')[1];
+      return $http.get('/api/talks/' + $scope.talk.id + '/transcripts/txt?lang=en');
+    })
+    .then(function (response) {
+      $scope.talk.transcript = '<p>' + response.data.split('\n\n').join('</p><p>') + '</p>';
+    });
 })
