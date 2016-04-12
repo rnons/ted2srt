114adf94fd84381e679acb8959c114e5762e4a71
rnons <remotenonsense@gmail.com>
2016-04-12 17:15:05 +0800

    pg_dump -d ted2srt -c -s -x > sql/latest.sql

---
+++ server/sql/latest.sql
@@ -0,0 +1,84 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.2
+-- Dumped by pg_dump version 9.5.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET search_path = public, pg_catalog;
+
+ALTER TABLE ONLY public.talks DROP CONSTRAINT talks_pkey;
+DROP TABLE public.talks;
+DROP EXTENSION plpgsql;
+DROP SCHEMA public;
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA public;
+
+
+ALTER SCHEMA public OWNER TO postgres;
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: talks; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE talks (
+    id smallint NOT NULL,
+    name text,
+    slug text,
+    filmed timestamp without time zone,
+    published timestamp without time zone
+);
+
+
+ALTER TABLE talks OWNER TO postgres;
+
+--
+-- Name: talks_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY talks
+    ADD CONSTRAINT talks_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
