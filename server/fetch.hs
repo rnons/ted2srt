b70d0f4886ec7392a59cb6fc5a649fd734e45cae
rnons <remotenonsense@gmail.com>
2016-04-21 13:02:24 +0800

    rewrite fetch.hs, saveToRedis -> saveToDB

--- server/fetch.hs
+++ server/fetch.hs
@@ -1,40 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-import           Control.Monad (forM_, void)
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson (decodeStrict, encode)
+import           Control.Monad (void, zipWithM)
+import           Data.Aeson (decodeStrict)
 import qualified Data.ByteString.Char8 as C
-import qualified Data.ByteString.Lazy as L
 import           Data.Maybe (catMaybes, mapMaybe)
 import           Data.Monoid ((<>))
+import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import           Data.Time (getCurrentTime)
-import           Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
 import           Database.Redis ( connect, defaultConnectInfo, runRedis
-                                , multiExec, get, set, del, mget, rpush, zadd
-                                , connectDatabase)
+                                , mget, connectDatabase)
+import           LoadEnv (loadEnv)
 import           Network.HTTP.Conduit (simpleHttp)
 import           Prelude hiding (id)
 import qualified Prelude
 import           Text.HTML.DOM (parseLBS)
-import qualified Text.XML as X
 import           Text.XML.Cursor
 
-import ReTed.Types (RedisTalk(..), marshal)
-import Web.TED hiding (content, publishedAt)
+import           ReTed.Config (getConfig)
+import qualified ReTed.Models.Talk as Talk
+import           ReTed.Types (RedisTalk(..))
+import           Web.TED hiding (content, publishedAt)
 
 
 type TalkId = Int
 
 main :: IO ()
 main = do
+    loadEnv
     res <- simpleHttp rurl
+    config <- getConfig
     let cursor = fromDocument $ parseLBS res
         tids = take limit (parseTids cursor)
+        urls = take limit (parseUrl cursor)
 
-    saveToRedis tids
-    X.writeFile X.def "atom.xml" . template =<< mkFeed =<< saveAsFeed tids
+    void $ zipWithM (Talk.saveToDB config) tids urls
+    -- X.writeFile X.def "atom.xml" . template =<< mkFeed =<< saveAsFeed tids
   where
     limit = 5
     rurl = "http://feeds.feedburner.com/tedtalks_video"
@@ -42,31 +44,10 @@ main = do
     parseTids :: Cursor -> [TalkId]
     parseTids cur = map (read . T.unpack) $ cur $// element "jwplayer:talkId"
                                                 &// content
+    parseUrl :: Cursor -> [Text]
+    parseUrl cur = cur $// element "feedburner:origLink"
+                       &// content
 
-saveToRedis :: [TalkId] -> IO ()
-saveToRedis tids = do
-    conn <- connect defaultConnectInfo { connectDatabase = 1 }
-    runRedis conn $ multiExec $
-        del [key] >> rpush key (map (C.pack . show) tids)
-    forM_ tids $ \tid -> do
-        mtalk <- runRedis conn $ get (C.pack $ show tid)
-        case mtalk of
-            Right (Just _) -> return ()
-            Right Nothing  -> do
-                talk' <- liftIO $ queryTalk tid
-                case talk' of
-                    Nothing -> return ()
-                    Just talk -> do
-                        dbtalk <- liftIO $ marshal talk
-                        void $ runRedis conn $ multiExec $ do
-                            set (C.pack $ T.unpack $ Web.TED.slug talk)
-                                (C.pack $ show tid)
-                            set (C.pack $ show tid)
-                                (L.toStrict $ encode dbtalk)
-                            zadd "tids" [(realToFrac $ utcTimeToPOSIXSeconds $ publishedAt dbtalk, C.pack $ show tid)]
-            Left err        -> error $ show err
-  where
-    key = "latest"
 
 talkToFeedEntry :: RedisTalk -> IO (Maybe FeedEntry)
 talkToFeedEntry RedisTalk {..} = do
