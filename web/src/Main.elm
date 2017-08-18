340738a4cd8b3b783f8b44ecf73ea2e25a71ca27
rnons <remotenonsense@gmail.com>
2017-08-18 19:08:52 +0900

    init api request

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onSubmit)
+import Http
+import Json.Decode as Decode
 import CssModules exposing (css)
 
 
@@ -14,33 +16,45 @@ import CssModules exposing (css)
 
 
 main =
-    beginnerProgram { model = model, view = view, update = update }
+    program { init = init, view = view, update = update, subscriptions = subscriptions }
 
 
 type alias Model =
     { url : String
+    , talks : List Talk
     }
 
 
-model : Model
-model =
-    { url = ""
-    }
+init : ( Model, Cmd Msg )
+init =
+    ( Model "" [], getTalks )
 
 
 type Msg
     = Input String
     | Submit
+    | TalksResult (Result Http.Error (List Talk))
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Input text ->
-            { model | url = text }
+            ( { model | url = text }, Cmd.none )
 
         Submit ->
-            { model | url = model.url ++ " SUBMITTED" }
+            ( { model | url = model.url ++ " SUBMITTED" }, Cmd.none )
+
+        TalksResult (Ok talks) ->
+            ( { model | talks = talks }, Cmd.none )
+
+        TalksResult (Err _) ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 view : Model -> Html Msg
@@ -61,4 +75,34 @@ view model =
                 ]
                 []
             ]
+        , div [] (talksView model.talks)
         ]
+
+
+talksView : List Talk -> List (Html Msg)
+talksView talks =
+    talks
+        |> List.map (\talk -> div [] [ text talk.name ])
+
+
+type alias Talk =
+    { name : String
+    }
+
+
+getTalks : Cmd Msg
+getTalks =
+    let
+        url =
+            "/api/talks?limit=5"
+    in
+        Http.send TalksResult (Http.get url decodeTalks)
+
+
+talkDecoder =
+    Decode.map Talk (Decode.field "name" Decode.string)
+
+
+decodeTalks : Decode.Decoder (List Talk)
+decodeTalks =
+    Decode.list talkDecoder
