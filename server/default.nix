0213e6d2115220c446d5dae0314cc143b75dc82c
Ping Chen <remotenonsense@gmail.com>
2018-04-27 11:38:48 +0900

    add default.nix

---
+++ server/default.nix
@@ -0,0 +1,22 @@
+{nixpkgs ? import <nixpkgs> { }, ghc ? nixpkgs.ghc}:
+
+with nixpkgs;
+
+let
+  nativeLibs = [
+    gmp
+    postgresql
+    redis
+    zlib
+  ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+    Cocoa
+    CoreServices
+  ]);
+
+in haskell.lib.buildStackProject {
+  inherit ghc;
+
+  name = "ted2srt";
+
+  buildInputs = nativeLibs;
+}
