66ff040099dcb04fe3f158b73ef6d5d5c857f232
Ping Chen <remotenonsense@gmail.com>
2018-07-06 10:53:39 +0900

    [be] pass talk object to the frontend on Talk page

--- backend/src/View/Talk.hs
+++ backend/src/View/Talk.hs
@@ -1,20 +1,28 @@
 module View.Talk where
 
+import           Data.Aeson      (encode)
+import           Data.Aeson.Text (encodeToLazyText)
+import qualified Data.Text.Lazy  as LT
 import           Lucid
+import           Models.Talk     (getTalkBySlug)
 import           RIO
-import           Servant     (err404)
 import           Types
 import           View.Bundle
 import           View.Error
 
 getTalkH :: Text -> AppM (Html ())
 getTalkH slug = do
-  get404H
-  bundle <- includeBundle TalkBundle
-  pure $ doctypehtml_ $ do
-    head_ $ do
-      meta_ [charset_ "utf-8"]
-      meta_ [name_ "viewport"
-            ,content_ "width=device-width, initial-scale=1"]
-    body_ $ do
-      bundle
+  mTalk <- lift $ getTalkBySlug slug
+  case mTalk of
+    Nothing   -> get404H
+    Just talk -> do
+      bundle <- includeBundle TalkBundle
+      pure $ doctypehtml_ $ do
+        head_ $ do
+          meta_ [charset_ "utf-8"]
+          meta_ [name_ "viewport"
+                ,content_ "width=device-width, initial-scale=1"]
+          script_ $ LT.toStrict $
+            "window.TALK = " <> encodeToLazyText talk
+        body_ $ do
+          bundle
