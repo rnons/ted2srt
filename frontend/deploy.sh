c102c9a778370afa0712bea3fb777309afcee4d6
Ping Chen <remotenonsense@gmail.com>
2018-07-08 13:42:47 +0900

    [ops] change frontend/deploy.sh a bit

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 rm -rf dist
-NODE_ENV=production npm run build
+NODE_ENV=production yarn build
 cp src/search.xml dist
 tar czf dist.tar.gz dist
 scp dist.tar.gz ted2srt:/tmp/
 ssh ted2srt /bin/sh <<'ENDSSH'
 cd /tmp
 tar xf dist.tar.gz
-rsync -a --delete dist/ /var/www/ted2srt/app
+rsync -a --delete dist/ /var/www/ted2srt/dist
 ENDSSH
 rm dist.tar.gz
