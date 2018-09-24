2c768d5499178335313c280ddf13bfd3d2bd5208
Ping Chen <remotenonsense@gmail.com>
2018-09-24 16:20:50 +0900

    [be] clean up some unused TED api code

--- backend/src/Models/Types.hs
+++ backend/src/Models/Types.hs
@@ -1,64 +1,6 @@
-{-# LANGUAGE DuplicateRecordFields #-}
-
 module Models.Types where
 
-import           Data.Aeson
-import           Data.Text    (Text)
-import           Data.Time    (UTCTime)
-import           GHC.Generics (Generic)
-import           RIO          hiding (id)
-
-import qualified Web.TED      as TED
+import           RIO
 
 mkTalkUrl :: Text -> Text
 mkTalkUrl s = "http://www.ted.com/talks/" <> s
-
-marshal :: TED.Talk -> IO RedisTalk
-marshal talk@(TED.Talk{..}) = do
-  (mediaSlug, mediaPad) <- TED.getSlugAndPad $ mkTalkUrl $ TED.slug (talk :: TED.Talk)
-  return RedisTalk
-    { mediaSlug = mediaSlug
-    , mediaPad = mediaPad
-    , ..
-    }
-
-data RedisTalk = RedisTalk
-    { id          :: Int
-    , name        :: Text
-    , description :: Text
-    , slug        :: Text
-    , images      :: TED.Image
-    , publishedAt :: UTCTime
-    , mediaSlug   :: Text
-    , mediaPad    :: Double
-    } deriving (Generic, Show)
-instance FromJSON RedisTalk
-instance ToJSON RedisTalk
-
-data TalkCache = TalkCache
-    { caLanguages :: [TED.Language]
-    , caAudio     :: Bool
-    } deriving (Generic, Show)
-instance FromJSON TalkCache
-instance ToJSON TalkCache
-
--- data TalkResp = TalkResp RedisTalk [TED.Language]
-data TalkResp = TalkResp RedisTalk TalkCache
-instance ToJSON TalkResp where
-    toJSON (TalkResp talk cache) =
-        object [ "id" .= id talk
-               , "name" .= name talk
-               , "description" .= description talk
-               , "slug" .= slug talk
-               , "images" .= images talk
-               , "publishedAt" .= publishedAt talk
-               , "mediaSlug" .= mediaSlug talk
-               , "languages" .= caLanguages cache
-               , "hasAudio" .= caAudio cache
-               ]
-
-tedTalkToCache :: TED.Talk -> TalkCache
-tedTalkToCache talk =
-    TalkCache { caLanguages = TED.languages talk
-              , caAudio = TED.talkHasAudio talk
-              }
--- backend/src/Web/TED/API.hs
+++ backend/src/Web/TED/API.hs
@@ -2,83 +2,17 @@
 -- Documented at <http://developer.ted.com/io-docs>
 
 module Web.TED.API
-  ( Talk (..)
-  , SearchTalk (..)
-  , Image (..)
-  , Language (..)
-  , queryTalk
-  , searchTalk
-  , talkHasAudio
-  , getTalkTranscript
+  ( getTalkTranscript
   ) where
 
-import qualified Control.Exception     as E
-import           Data.Aeson
-import           Data.Aeson.Types      (Options (..), defaultOptions)
-import qualified Data.ByteString.Char8 as B8
-import qualified Data.HashMap.Strict   as HM
-import           Data.Maybe            (isJust)
-import           Data.Text             (Text)
-import qualified Data.Text             as T
-import           GHC.Generics          (Generic)
-import           Network.HTTP.Conduit  (simpleHttp)
-import           Network.HTTP.Types    (urlEncode)
+import           Data.Aeson           (eitherDecode)
+import           Data.Text            (Text)
+import qualified Data.Text            as T
+import           Network.HTTP.Conduit (simpleHttp)
 import           RIO
-import           System.IO             (print)
 
 import           Web.TED.Types
 
-
--- | Response of https://api.ted.com/v1/talks/:id.json
-data QueryResponse = QueryResponse
-    { talk          :: Talk
-    } deriving (Generic, Show)
-instance FromJSON QueryResponse
-
--- | Response of https://api.ted.com/v1/search.json
-data SearchResponse = SearchResponse
-    { results       :: [SearchResult]
-    } deriving (Generic, Show)
-instance FromJSON SearchResponse
-
-data SearchResult = SearchResult
-    { _talk       :: SearchTalk
-    } deriving (Generic, Show)
-instance FromJSON SearchResult where
-    parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 1 }
-
--- | Return Nothing for talks hosted externally (youtube, vimeo), e.g. 720.
--- They have no downloadable subtitles and will fail getSlugAndPad.
-queryTalk :: Int -> IO (Maybe Talk)
-queryTalk tid = E.catch
-    (do
-        res <- simpleHttp rurl
-        case eitherDecode res of
-            Right r -> return $ Just $ talk r
-            Left er -> error er)
-    (\e -> print (e :: E.SomeException) >> return Nothing)
-  where
-    rurl = "http://api.ted.com/v1/talks/" ++ show tid ++
-           ".json?external=false&podcasts=true&api-key=2a9uggd876y5qua7ydghfzrq"
-
--- | Whether "audio-podcast" field is present
-talkHasAudio :: Talk -> Bool
-talkHasAudio t =
-    case media t of
-        Object ms -> isJust $ HM.lookup "internal" ms >>=
-                            \(Object im) -> HM.lookup "audio-podcast" im
-        _         -> False
-
-searchTalk :: Text -> IO [SearchTalk]
-searchTalk q = do
-    res <- simpleHttp rurl
-    case eitherDecode res of
-        Right r -> return $ map _talk (results r)
-        Left er -> error er
-  where
-    query = B8.unpack $ urlEncode True $ B8.pack $ T.unpack q
-    rurl = "http://api.ted.com/v1/search.json?q=" <> query <>
-           "&categories=talks&api-key=2a9uggd876y5qua7ydghfzrq"
 
 getTalkTranscript :: Int -> Text -> IO Text
 getTalkTranscript talkId language = do
