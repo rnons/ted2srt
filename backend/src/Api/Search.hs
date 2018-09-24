8615b98e827167d958bc74f5a50e2d019a77c989
Ping Chen <remotenonsense@gmail.com>
2018-09-24 16:02:24 +0900

    [be] use searchTalkFromDb only

--- backend/src/Api/Search.hs
+++ backend/src/Api/Search.hs
@@ -3,24 +3,20 @@ module Api.Search
   ) where
 
 import           Config
-import           Control.Monad.Except             (catchError)
 import qualified Data.Text                        as T
 import qualified Database.PostgreSQL.Simple       as Pg
 import           Database.PostgreSQL.Simple.SqlQQ (sql)
 import           Model
-import           Models.Talk
 import           RIO
 import           Servant                          (err400, throwError)
 import           Types                            (AppM)
-import           Web.TED                          (SearchTalk (..))
-import qualified Web.TED                          as TED
 
 searchTalkFromDb :: Text -> AppM [Talk]
 searchTalkFromDb q = do
   Config { dbConn } <- ask
   liftIO $ Pg.query dbConn [sql|
-      SELECT talks.* FROM talks JOIN transcripts
-      ON talks.id = transcripts.id
+      SELECT talk.* FROM talk JOIN transcript
+      ON talk.id = transcript.id
       WHERE en_tsvector @@
             to_tsquery('english', ?)
       |] [query]
@@ -28,15 +24,7 @@ searchTalkFromDb q = do
     query = T.intercalate "&" $ T.words q
 
 searchTalk :: Text -> AppM [Talk]
-searchTalk q = do
-  (do
-    searchResults <- liftIO $ TED.searchTalk q
-    liftM catMaybes $ forM searchResults $ \SearchTalk{slug} -> do
-      lift $ getTalkBySlug slug
-    ) `catchError`
-    (\e ->
-      logErrorS "searchTalk" (displayShow e) >> searchTalkFromDb q
-    )
+searchTalk = searchTalkFromDb
 
 getSearchApiH :: Maybe Text -> AppM [Talk]
 getSearchApiH (Just q) = searchTalk q
