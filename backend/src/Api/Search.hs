3fb44aa6d1f3d777f781389c8c7afda38f655944
Ping Chen <remotenonsense@gmail.com>
2018-09-27 15:15:24 +0900

    [be] use ilike instead of tsquery

--- backend/src/Api/Search.hs
+++ backend/src/Api/Search.hs
@@ -3,7 +3,6 @@ module Api.Search
   ) where
 
 import           Config
-import qualified Data.Text                        as T
 import qualified Database.PostgreSQL.Simple       as Pg
 import           Database.PostgreSQL.Simple.SqlQQ (sql)
 import           Model
@@ -11,20 +10,28 @@ import           RIO
 import           Servant                          (err400, throwError)
 import           Types                            (AppM)
 
-searchTalkFromDb :: Text -> AppM [Talk]
-searchTalkFromDb q = do
-  Config { dbConn } <- ask
-  liftIO $ Pg.query dbConn [sql|
-      SELECT talk.* FROM talk JOIN transcript
-      ON talk.id = transcript.id
-      WHERE en_tsvector @@
-            to_tsquery('english', ?)
-      |] [query]
-  where
-    query = T.intercalate "&" $ T.words q
+-- searchTalkFromDb :: Text -> AppM [Talk]
+-- searchTalkFromDb q = do
+--   Config { dbConn } <- ask
+--   liftIO $ Pg.query dbConn [sql|
+--       SELECT talk.* FROM talk JOIN transcript
+--       ON talk.id = transcript.id
+--       WHERE en_tsvector @@
+--             to_tsquery('english', ?)
+--       |] [query]
+--   where
+--     query = T.intercalate "&" $ T.words q
 
 searchTalk :: Text -> AppM [Talk]
-searchTalk = searchTalkFromDb
+searchTalk q = do
+  Config { dbConn } <- ask
+  liftIO $ Pg.query dbConn [sql|
+    SELECT * FROM talk
+    WHERE name ilike ? or
+          description ilike ?
+    |] [query, query]
+  where
+    query = "%" <> q <> "%"
 
 getSearchApiH :: Maybe Text -> AppM [Talk]
 getSearchApiH (Just q) = searchTalk q
