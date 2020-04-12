9aeb2cbd39a13245cca323177ad066aadf65e4ed
Ping Chen <remotenonsense@gmail.com>
2020-04-12 13:02:05 +0900

    git add backend/ted2srt.nix

--- backend/scripts/cabal2nix.sh
+++ backend/scripts/cabal2nix.sh
@@ -2,4 +2,3 @@ hpack
 cabal2nix --no-haddock . > ted2srt.nix
 sed -i '0,/}/ s/}/, postgresql_11}/' ted2srt.nix
 sed -i '0,/.*libraryToolDepends.*/ s/.*libraryToolDepends.*/  librarySystemDepends = [ postgresql_11 ];\n&/' ted2srt.nix
-nix-build
---
+++ backend/ted2srt.nix
@@ -0,0 +1,40 @@
+{ mkDerivation, aeson, base, base64-bytestring, bytestring, conduit
+, conduit-extra, containers, cryptonite, cryptonite-conduit
+, directory, either, hedis, hpack, html-conduit, http-conduit
+, http-types, load-env, lucid, memory, monad-logger, mtl, network
+, persistent, persistent-postgresql, persistent-template
+, raw-strings-qq, regex-posix, rio, servant-lucid, servant-server
+, stdenv, system-filepath, text, time, transformers
+, unordered-containers, vector, wai, wai-extra, warp, xml-conduit
+, postgresql_11}:
+mkDerivation {
+  pname = "ted2srt";
+  version = "3.20200412";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson base base64-bytestring bytestring conduit conduit-extra
+    containers cryptonite cryptonite-conduit directory either hedis
+    html-conduit http-conduit http-types load-env lucid memory
+    monad-logger mtl network persistent persistent-postgresql
+    persistent-template raw-strings-qq regex-posix rio servant-lucid
+    servant-server system-filepath text time transformers
+    unordered-containers vector wai xml-conduit
+  ];
+  librarySystemDepends = [ postgresql_11 ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    aeson base base64-bytestring bytestring conduit conduit-extra
+    containers cryptonite cryptonite-conduit directory either hedis
+    html-conduit http-conduit http-types load-env lucid memory
+    monad-logger mtl network persistent persistent-postgresql
+    persistent-template raw-strings-qq regex-posix rio servant-lucid
+    servant-server system-filepath text time transformers
+    unordered-containers vector wai wai-extra warp xml-conduit
+  ];
+  doHaddock = false;
+  prePatch = "hpack";
+  license = "unknown";
+  hydraPlatforms = stdenv.lib.platforms.none;
+}
