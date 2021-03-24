7456f109bce2b9f07d0c929bef2fd42e6bc4f75d
Ping Chen <remotenonsense@gmail.com>
2021-03-24 17:26:00 +0900

    Set RTS -N2 to zephyr to make circleci happy

--- frontend/scripts/build.sh
+++ frontend/scripts/build.sh
@@ -4,6 +4,6 @@ rm -rf ../backend/dist/*.{css,js}
 
 spago build -u "-g corefn +RTS -N2 -RTS"
 
-zephyr -f Home.main Talk.main Search.main
+zephyr -f Home.main Talk.main Search.main +RTS -N2 -RTS
 
 NODE_ENV=production webpack -p --progress
